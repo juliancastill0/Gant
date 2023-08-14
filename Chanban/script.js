9442b266f8318fa86510ebc0df6602aff9d7ef81
beluarias <belenariasfor@gmail.com>
2023-08-14 01:21:41 -0300

    Ahora si final

--- Chanban/script.js
+++ Chanban/script.js
@@ -1,6 +1,14 @@
-// para que al tocar una imagen abra en pantalla completa
-const imagen = document.getElementById('imgcomponentes');
+const imagenes = document.querySelectorAll('img');
 
-imagen.addEventListener('click', () => {
-  imagen.classList.toggle('ampliada');
-});
+for (let index = 0; index < imagenes.length; index++) {
+  imagenes[index].addEventListener('click', () => {
+    imagenes[index].classList.toggle('ampliada');
+  });
+
+  imagenes[index].addEventListener('wheel', (e) => {
+    if (imagenes[index].classList.contains('ampliada')) {
+      const escala = e.deltaY > 0 ? 1.1 : 0.9; // Ajustar el factor de zoom
+      imagenes[index].style.transform = `scale(${escala})`;
+    }
+  });
+}
